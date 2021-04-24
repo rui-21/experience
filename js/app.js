fafcf9e6388c4f8891429774cf7eddaa079032cc
rui-21 <reisu6235@gmail.com>
2021-04-24 22:07:08 +0900

    [Start]java-3

--- js/app.js
+++ js/app.js
@@ -1,16 +1,9 @@
-int1 = 10;
-int2 = 4;
+var alertString;
+alertString = addString("WebCamp");
 
-result = int1 + int2
-alert('足し算の結果は' + result + 'です');
+alert(alertString);
 
-result = int1 - int2
-alert('引き算の結果は' + result + 'です');
-
-result = int1 * int2
-alert('掛け算の結果は' + result + 'です');
-
-result = int1 / int2
-alert('割り算の結果は' + result + 'です');
-
-alert('以上です')
+function addString(strA){
+  var addStr = "Hello " + strA;
+  return addStr;
+}
