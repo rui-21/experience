62804b1c9d040d2c3ed90de28b6e345010c5fb86
rui-21 <reisu6235@gmail.com>
2021-04-24 21:19:28 +0900

    [Update]java-2

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-var int1 = 1;
-alert(int1)
+var max = 1000;
+var num = 1;
+var count = 0;
+
+while(num < max){
+  num = num * 3;
+  count = count + 1;
+}
+
+alert('3を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
